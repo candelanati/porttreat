5f3f2c6b495f2b200c2ce87e87273e0822194841
problablemente <candelanati23@gmail.com>
2024-03-13 20:29:38 -0300

    animacion logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,15 @@ export default {
       fontFamily: {
         sans: ['Outfit Variable', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        appear: {
+          '0% , 50%': { opacity: '0.1' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'port-logo': 'appear .5s linear ',
+      },
     },
   },
   plugins: [],
