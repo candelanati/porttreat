d5a9fbe937df32bb45011286bd97eea756916415
problablemente <candelanati23@gmail.com>
2024-07-10 17:08:49 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),     
-    tailwind({
-    applyBaseStyles: false,
-  }),
-],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
