7f6f9f9ac4cffe42eb617627496de58104fc3eb5
candelanati <candelanti23@gmail.com>
2024-07-12 15:02:46 -0300

    accordion shadcn funcional

---
+++ src/components/MyAccordion.tsx
@@ -0,0 +1,23 @@
+// src/components/Accordion.tsx
+import React from 'react';
+import { Accordion as BaseAccordion, AccordionContent, AccordionItem, AccordionTrigger } from '../components/ui/accordion';
+
+interface Props {
+  data: {
+    title: string;
+    description: string;
+  }[];
+}
+
+export function Accordion({ data = [] }: Props) {
+  return (
+    <BaseAccordion type="single" collapsible className="w-full">
+      {data.map((item, i) => (
+        <AccordionItem key={i} value={`item-${i}`}>
+          <AccordionTrigger>{item.title}</AccordionTrigger>
+          <AccordionContent>{item.description}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </BaseAccordion>
+  );
+}
